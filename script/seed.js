5f5749b3c60bd9511c8500bee696926bc42fe37b
Jing Mo <Jingmo39@gmail.com>
2023-03-15 16:48:41 -0400

    authForm

--- script/seed.js
+++ script/seed.js
@@ -12,8 +12,8 @@ async function seed() {
 
   // Creating Users
   const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
+    User.create({ username: 'cody', password: '123', email: 'hello@gmail.com' }),
+    User.create({ username: 'murphy', password: '123', email: 'hellomurphy@gmail.com' }),
   ])
 
   console.log(`seeded ${users.length} users`)
