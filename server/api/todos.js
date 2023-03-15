4eae11485b4520c926aba56e8c3b633677f7c89b
Zelda Ogiamien <zel0789@yahoo.com>
2023-03-15 17:24:10 -0400

    todo route

---
+++ server/api/todos.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const {
+  models: { ToDo },
+} = require("../db");
+
+// GET
+
+// POST
+
+// DELETE
+
+module.exports = router;
