063aedd7f9544676a58d02b87d3e47f0b866f4b4
Carla Herrera <cgherrera07@gmail.com>
2023-03-15 20:56:03 -0400

    Edit require statement for models to match main branch

--- server/api/pets.js
+++ server/api/pets.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-const {
-  models: { User, ToDo, Pet },
-} = require("../db");
+const { User, ToDo, Pet } = require("../db");
 
 router.get("/:id", async (req, res, next) => {
   try {
