b4ef101b831eee8f01debc3dfb4668e19498207b
Jing Mo <Jingmo39@gmail.com>
2023-03-16 15:38:52 -0400

    form layout

--- client/app/store.js
+++ client/app/store.js
@@ -1,14 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import logger from 'redux-logger';
-import authReducer from '../features/auth/authSlice';
-import petSlice from '../features/pet/petSlice';
-import singleUserSlice from '../features/user/userSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+import authReducer from "../features/auth/authSlice";
+import petSlice from "../features/pet/petSlice";
+import singleUserSlice from "../features/user/userSlice";
 
 const store = configureStore({
-  reducer: { auth: authReducer,
-  pet: petSlice, user: singleUserSlice },
+  reducer: {
+    auth: authReducer,
+    pet: petSlice,
+    user: singleUserSlice,
+  },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
-export * from '../features/auth/authSlice';
+export * from "../features/auth/authSlice";
