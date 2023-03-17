af9b74f7fbd3d4237104f7ac77d65cd884817ee9
Carla Herrera <cgherrera07@gmail.com>
2023-03-17 10:28:10 -0400

    Fix typo with dispatch and add About Me form

--- client/features/user/userPage.js
+++ client/features/user/userPage.js
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { selectSingleUser, fetchSingleUser } from "./userSlice"
+import { selectSingleUser, fetchSingleUser } from "./userSlice";
 
-//This component is unfinished, still need to add details and make sure it works 
+//This component is unfinished, still need to add details and make sure it works
 const User = () => {
   const dispatch = useDispatch();
 
@@ -10,39 +10,38 @@ const User = () => {
   const { displayName, profilePic, pronouns } = singleUser;
 
   useEffect(() => {
-    dispatchEvent(fetchSingleUser(id));
+    dispatch(fetchSingleUser(id));
   }, [dispatch]);
 
   return (
     <div>
       <div id="user-profile">
-          <div>
-            <header id="user-header">
-              <img
-                id="user-img"
-                src={profilePic}
-              ></img>
-              <div>
-                <h2>Name: {displayName}</h2>
-                  <p>Pronouns: {pronouns}</p>
-              </div>
-            </header>
-
-            <div id="pet-details">
-                Pet: {pet.name}
-                <img id="pet-img">
-                    {pet.image}
-                </img>
-                Points: {pet.experience}
+        <div>
+          <header id="user-header">
+            <img id="user-img" src={profilePic}></img>
+            <div>
+              <h2>Name: {displayName}</h2>
+              <p>Pronouns: {pronouns}</p>
+              <form id="aboutMe-Form">
+                <label htmlFor="displayName">
+                  <small>About Me</small>
+                </label>
+                <input name="aboutMe" type="text" />
+              </form>
             </div>
+          </header>
 
-            <div id="todos-left">
-                {/* {todos} */}
-            </div>
+          <div id="pet-details">
+            Pet: {pet.name}
+            <img id="pet-img">{pet.image}</img>
+            Points: {pet.experience}
           </div>
-     </div>
-     </div>
-     )
-  };
+
+          <div id="todos-left">{/* {todos} */}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default User;
