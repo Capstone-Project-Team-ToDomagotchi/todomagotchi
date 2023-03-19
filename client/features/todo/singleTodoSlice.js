33e0e8548e5d623762d67cbfc1c84587e028cc56
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-19 14:00:57 -0400

    add additional functionality to todo slice

--- client/features/todo/singleTodoSlice.js
+++ client/features/todo/singleTodoSlice.js
@@ -1,17 +1,40 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+//get the user's single todo
 export const fetchSingleTodo = createAsyncThunk("singleTodo", async (id) => {
     try {
         const { data } = await axios.get(`/api/todos/${id}`);
         return data;
     } catch (err) {
-        console.log(err)
+        console.error(err)
     }
 });
 
+//edit a single todo
+export const editSingleTodo = createAsyncThunk('editSingleTodo', async({id, dueDate, toDoName, pointType, description, isCompleted}) => {
+    try {
+        const { data } = await axios.put(`/api/todos/${id}`, {dueDate, toDoName, description, pointType,isCompleted,});
+        return data;
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+//add a single todo
+export const addSingleTodo = createAsyncThunk('addSingleTodo', async({id, dueDate, toDoName, pointType, description, isCompleted}) => {
+    try {
+        const { data } = await axios.post(`/api/todos/${id}`, {dueDate, toDoName, description, pointType,isCompleted,});
+        return data;
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+//set state
 const initialState = {};
 
+//slices
 export const singleTodoSlice = createSlice({
     name: 'singleTodo',
     initialState,
@@ -19,6 +42,12 @@ export const singleTodoSlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(fetchSingleTodo.fulfilled, (state, action) => {
             return action.payload;
+        })
+        builder.addCase(editSingleTodo.fulfilled, (state, action) => {
+            return action.payload;
+        })
+        builder.addCase(addSingleTodo.fulfilled, (state, action) => {
+            state.push(action.payload)
         })
     }
 });
--- client/features/todo/todoSlice.js
+++ client/features/todo/todoSlice.js
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
+//get the user's list of todos
 export const fetchTodosAsync = createAsyncThunk('todos', async() => {
     try {
         const { data } = await axios.get(`/api/todos`)
