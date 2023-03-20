53a8eb2bb9cc9154f40b1da46e87e621914827e9
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-20 16:53:52 -0400

    change todo list so todos are clickable

--- client/features/todo/todoList.js
+++ client/features/todo/todoList.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { selectTodo, fetchTodosAsync } from "./todoSlice";
-import TodoCard from "./todoCard";
+
 
 const Todos = () => {
     const dispatch = useDispatch();
@@ -15,12 +15,16 @@ const Todos = () => {
     return (
         <div className="todo-container">
             <Link to="/addNewTodo">Add a new task</Link>
-            {todos ? (
-            todos.map((todo) => {
-                return <TodoCard key={todo.id} todo={todo} />;
-        })) : (
-        <p>Loading</p>)}
-        </div>
-    )
-};
+            {todos.map((todo) => (
+                <div key={todo.id}>
+                    <Link to={`/todos/${todo.id}`}>
+                    <h3>To Do: {todo.toDoName}</h3>
+                    <h4>Due Date: {todo.dueDate}</h4>
+                    <h5>{todo.isCompleted}</h5>
+                    </Link>
+                    </div>
+                ))}
+            </div>
+     )}
+
 export default Todos;
