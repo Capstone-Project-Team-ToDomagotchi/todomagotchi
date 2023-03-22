56190e5830d471b48ba0a6076713e92262c5d950
Carla Herrera <cgherrera07@gmail.com>
2023-03-22 11:31:56 -0400

    Edit editUser component to add functionality

--- client/features/user/editUser.js
+++ client/features/user/editUser.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch} from "react-redux";
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
@@ -9,36 +9,57 @@ const EditUser = () => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const userName = event.target.userName.value;
-        const displayName = event.target.displayName.value;
-        const pronouns = event.target.pronouns.value;
-        const profilePic = event.target.profilePic.value;
-        const aboutMe = event.target.aboutMe.value;
-
-    dispatch({
-        type: "users/${id}/edit",
-        payload:{userName, displayName, pronouns, profilePic, aboutMe}});
-        navigate(`/users/${id}`)
-    }
-
+    const [displayName, setDisplayName] = useState("");const [userName, setUserName] = useState("");
+    // const [profilePic, setProfilePic] = useState("");
+    const [pronouns, setPronouns] = useState("");
+    const [aboutMe, setAboutMe] = useState("");
+  
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      await dispatch(editSingleUser({ id, userName, displayName, pronouns, aboutMe }));
+      setUserName("");
+      setDisplayName("");
+    //   setProfilePic("");
+      setPronouns("");
+      setAboutMe("");
+      navigate(`/users/${id}`)
+    };
 
     return (
         <div className="edit-user">
             <form onSubmit={handleSubmit}>
                 <h3>Edit Information</h3>
                 <label htmlFor="displayName">New Name:</label>
-                <input type="text" name="displayName" />
+                <input 
+                type="text" 
+                name="displayName"
+                onChange={(event) => setDisplayName(event.target.value)} />
+
                 <label htmlFor="userName">New Username:</label>
-                <input type="text" name="userName" />
+                <input 
+                type="text" 
+                name="userName"
+                onChange={(event) => setUserName(event.target.value)} />
+
                 <label htmlFor="pronouns">Update Pronouns:</label>
-                <input type="text" name="pronouns" />
+                <input 
+                type="text" 
+                name="pronouns"
+                onChange={(event) => setPronouns(event.target.value)} />
+
                 <label htmlFor="aboutMe">Update About Me:</label>
-                <input type="text" name="aboutMe" />
+                <input 
+                type="text" 
+                name="aboutMe"
+                onChange={(event) => setAboutMe(event.target.value)} />
+
                 <label htmlFor="profilePic">Change Profile Picture</label>
-                <input name="profilePic" type="file" accept="image/*" />
+                <input 
+                name="profilePic" 
+                type="file" 
+                accept="image/*" />
                 <br></br>
+
                 <button type='submit'>Submit Changes</button>
                 <Link to='/users/:id'>Cancel</Link>
             </form>
