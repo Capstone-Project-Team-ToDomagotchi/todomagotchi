c2f19f2d222ae251ae7b1e0d253d2ce94f7c2caa
Carla Herrera <cgherrera07@gmail.com>
2023-03-23 14:13:21 -0400

    Move pet details section to own component

---
+++ client/features/user/petSnapshot.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, Link } from "react-router-dom";
+import { selectSingleUser, fetchSingleUser } from "./userSlice";
+
+const PetSnapshot = () => {
+  const dispatch = useDispatch();
+  const { id } = useParams();
+
+  const singleUser = useSelector(selectSingleUser);
+
+  const { selectPets } = singleUser;
+
+  useEffect(() => {
+    dispatch(fetchSingleUser(id));
+  }, [dispatch]);
+
+  return (
+    <div className="pet-details">
+      <div>
+        <p>List of Pets:</p>
+        {selectPets && selectPets.length ? (
+          selectPets.map((pet) => (
+            <div className="petList" key={pet.id}>
+              <Link to={`/pets/${pet.id}`}>
+                <h3>Name: {pet.name}</h3>
+              </Link>
+
+              <p>Age: {pet.age}</p>
+              <p>Description: {pet.description}</p>
+            </div>
+          ))
+        ) : (
+          <p>
+            <i>No pets exist for this user</i>
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PetSnapshot;
