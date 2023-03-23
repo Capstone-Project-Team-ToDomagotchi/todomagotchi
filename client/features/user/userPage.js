c46b9f9fb283c92f158368e2aba69a28b2662c2f
Carla Herrera <cgherrera07@gmail.com>
2023-03-23 14:14:21 -0400

    Import todos and pet snapshot components to improve readability of User page code

--- client/features/user/userPage.js
+++ client/features/user/userPage.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { selectSingleUser, fetchSingleUser } from "./userSlice";
+import TodosSnapshot from "./todosSnapshot";
+import PetSnapshot from "./petSnapshot";
 
 //Component to view User's profile
 const User = () => {
@@ -10,22 +12,7 @@ const User = () => {
 
   const singleUser = useSelector(selectSingleUser);
 
-  const {
-    displayName,
-
-    username,
-
-    profilePic,
-
-    pronouns,
-
-    pets,
-
-    selectPets,
-
-    todos,
-    aboutMe,
-  } = singleUser;
+  const { displayName, username, profilePic, pronouns, aboutMe } = singleUser;
 
   useEffect(() => {
     dispatch(fetchSingleUser(id));
@@ -35,6 +22,7 @@ const User = () => {
     <div>
       <div className="user-profile">
         <div>
+
           <header id="user-header">
             <img id="user-img" src={profilePic}></img>
             <div>
@@ -47,43 +35,15 @@ const User = () => {
               <Link to={`/users/${id}/edit`}>Edit Profile</Link>
             </div>
           </header>
+          
         </div>
       </div>
-      <div className="todo-details">
-        <p>List of Todos:</p>
-        {todos && todos.length ? (
-          todos.map((todo) => (
-            <div className="todoList" key={todo.id}>
-              <Link to={`/todos/${todo.id}`}>Name: {todo.todoName}</Link>
-              <br />
-              {todos.description}
-              <br />
-            </div>
-          ))
-        ) : (
-          <p>
-            <i>No todos exist for this user</i>
-          </p>
-        )}
-      </div>
-      <div className="pet-details">
-        <p>List of Pets:</p>
-        {selectPets && selectPets.length ? (
-          selectPets.map((pet) => (
-            <div className="petList" key={pet.id}>
-              <Link to={`/pets/${pet.id}`}>
-                <h3>Name: {pet.name}</h3>
-              </Link>
 
-              <p>Age: {pet.age}</p>
-              <p>Description: {pet.description}</p>
-            </div>
-          ))
-        ) : (
-          <p>
-            <i>No pets exist for this user</i>
-          </p>
-        )}
+      <div>
+        <TodosSnapshot />
+      </div>
+      <div>
+        <PetSnapshot />
       </div>
     </div>
   );
