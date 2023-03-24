5fee172b86033407a9234dbe7a18cdafd2750bc4
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-24 11:20:55 -0400

    remove faker code from seed files

--- script/selectPetData.js
+++ script/selectPetData.js
@@ -1,13 +1,4 @@
-const { faker } = require("@faker-js/faker");
 const { SelectPet } = require("../server/db");
-
-// function createSelectPetArray() {
-//   let selectPetArray = [];
-//   for (let i = 1; i < 10; i++) {
-//     selectPetArray.push(createSelectPet());
-//   }
-//   return selectPetArray;
-// }
 
 async function selectPetSeed() {
   const selectPets = await Promise.all([
@@ -61,30 +52,5 @@ async function selectPetSeed() {
     selectPets,
   };
 }
-
-// function createSelectPet() {
-//   const exp = faker.datatype.number({ min: 1, max: 100 });
-//   const userId = faker.datatype.number({ min: 1, max: 10 });
-//   const todoId = faker.datatype.number({ min: 1, max: 10 });
-//   const petId = faker.datatype.number({ min: 1, max: 5 });
-//   const name = faker.lorem.word({ length: { min: 1, max: 3 } });
-//   const age = faker.datatype.number({ min: 1, max: 5 });
-
-//   return {
-//     exp,
-//     userId,
-//     todoId,
-//     petId,
-//     age,
-//     name,
-//   };
-// }
-
-// async function selectPetSeed() {
-//   const selectPetArray = createSelectPetArray();
-//   const selectPet = await SelectPet.bulkCreate(selectPetArray);
-
-//   return { selectPet };
-// }
 
 module.exports = selectPetSeed;
