e1ea5562bfd86aa956601f8ade3545164170b303
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-24 11:18:11 -0400

    homepage splitting into 3 components

--- client/features/home/Home.js
+++ client/features/home/Home.js
@@ -1,34 +1,6 @@
-// import React from "react";
-// import { useSelector } from "react-redux";
-
-// import styles from "../styles/Home.module.css";
-
-// /**
-//  * COMPONENT
-//  */
-// const Home = (props) => {
-//   const username = useSelector((state) => state.auth.me.username);
-//   const profilePic = useSelector((state) => state.auth.me.profilePic);
-
-//   return (
-//     <div className={styles.welcome}>
-//       <h3>Welcome, {username}</h3>
-//       {/* <img src={profilePic} alt="Profile Picture" /> */}
- 
-//     </div>
-//   );
-// };
-
-// export default Home;
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import {
-  fetchPetByUserId,
-  selectSinglePet,
-  singlePetSlice,
-} from "../pet/petSlice";
-// import { fetchAllPetsAsync, selectAllPets } from "../pet/allPetsSlice";
+import { Link, useNavigate, useParams } from "react-router-dom"
 import { selectTodo, fetchTodosAsync } from "../todo/todoSlice";
 import { selectSingleUser, fetchSingleUser } from "../user/userSlice";
 
@@ -56,8 +28,7 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Welcome!</h1>
+    <div className={styles.Home}>
       <nav>
         {isLoggedIn ? (
           <div>
@@ -69,7 +40,7 @@ const Home = () => {
                   <Link to={`/pets/${pet.id}`}>
                     <h3>Name: {pet.name}</h3>
                   </Link>
-                  <img src={`${pet?.image}`} />
+                  <img src={`${pet.pet.image}`} />
                   <p>Age: {pet.age}</p>
                   <p>Description: {pet.description}</p>
                 </div>
@@ -81,26 +52,67 @@ const Home = () => {
             )}
             <div className="todo-container">
               <h2>Current To-Dos:</h2>
-              {todos.map((todo) => {
-                return (
-                  <div key={todo.id}>
-                    <h3>
-                      <Link to={`/todos/${todo.id}`}>
-                        To Do: {todo.todoName}
-                      </Link>
-                    </h3>
-                    <h4>Due Date: {todo.dueDate}</h4>
-                    <h5>{todo.isCompleted}</h5>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+              {todos && todos.length ? (
+                todos.map((todo) => (
+                  <div className="todoList" key={todo.id}>
+                <Link to={`/todos/${todo.id}`}>Name: {todo.todoName}</Link>
+                <br />
+                {todos.description}
+              </div>
+                ))
         ) : (
           <div>
             {/* The mainpage will show these elements only before you've signed in */}
             <Link to="/login">Login</Link>
             <Link to="/signup">Sign Up</Link>
+            <div>
+      <h1>Welcome to ToDomagotchi!</h1>
+      <div>
+        {/* Choose a pet section */}
+        <div>
+          <h2>Choose a pet!</h2>
+          <div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
+              corrupti ipsam modi at neque itaque, blanditiis fuga eligendi!
+              Porro, totam!
+            </p>
+          </div>
+          <div>
+            <div>
+              <p>Animal</p>
+              <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
+            </div>
+            <div>
+              <p>Plant</p>
+              <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
+            </div>
+          </div>
+        </div>
+        {/* Make your pet grow with productivity section */}
+        <div>
+          <h3>Make your pet grow with productivity!</h3>
+          <div>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
+              corrupti ipsam modi at neque itaque, blanditiis fuga eligendi!
+              Porro, totam!
+            </p>
+            <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
+          </div>
+          <div>
+            <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
+              corrupti ipsam modi at neque itaque, blanditiis fuga eligendi!
+              Porro, totam!
+            </p>
+          </div>
+        </div>
+        {/* Home pic on home page section */}
+        <div>
+          <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
+        </div>
           </div>
         )}
       </nav>
