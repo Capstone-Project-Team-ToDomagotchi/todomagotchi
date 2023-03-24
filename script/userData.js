6f0b1f4f98186825358ec0b2056b4aa890f8b663
Elizabeth Carter <betsymariecarter@gmail.com>
2023-03-24 11:10:52 -0400

    removed commented out code

--- script/userData.js
+++ script/userData.js
@@ -1,51 +1,6 @@
 const { faker } = require("@faker-js/faker");
 const { User } = require("../server/db");
 
-// function createUserArray() {
-//   let userArray = [];
-//   userArray.push(createTestUser());
-//   for (let i = 1; i < 10; i++) {
-//     userArray.push(createUser());
-//   }
-//   return userArray;
-// }
-// function createUser() {
-//   const username = faker.lorem.word({ min: 1, max: 5 });
-//   const password = faker.internet.password();
-//   const email = faker.internet.email();
-//   const pronouns = faker.helpers.arrayElement(
-//     ["she/her", "he/his", "they/them"],
-//     1
-//   );
-//   const displayName = faker.name.firstName();
-//   const image = faker.image.imageUrl();
-//   console.log({ username, password, email, pronouns, displayName, image });
-//   return {
-//     username,
-//     password,
-//     email,
-//     pronouns,
-//     displayName,
-//     image,
-//   };
-// }
-
-// function createTestUser() {
-//   const username = "Jin";
-//   const password = "456";
-//   const email = "sally123@todomagotchi.com";
-//   const pronouns = "she/her";
-//   const displayName = "jin123";
-
-//   return {
-//     username,
-//     password,
-//     email,
-//     pronouns,
-//     displayName,
-//   };
-// }
-
 async function userSeed() {
   const users = await Promise.all([
     User.create({
@@ -86,14 +41,5 @@ async function userSeed() {
   ]);
   return { users };
 }
-
-// async function userSeed() {
-//   const userArray = createUserArray();
-//   const users = await User.bulkCreate(userArray);
-
-//   return {
-//     users,
-//   };
-// }
 
 module.exports = userSeed;
