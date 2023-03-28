303ef523769ac7916f9ba9ba3908b9aade855ee7
Carla Herrera <cgherrera07@gmail.com>
2023-03-28 12:03:43 -0400

    Delete console.logs in file

--- client/features/user/singleUserSlice.js
+++ client/features/user/singleUserSlice.js
@@ -4,9 +4,7 @@ import axios from "axios";
 //Create thunk to fetch single user
 export const fetchSingleUser = createAsyncThunk("singleUser", async (id) => {
   try {
-    console.log('get single user')
     const { data } = await axios.get(`/api/users/${id}`);
-    console.log('got single user')
     return data;
   } catch (err) {
     console.error(err);
