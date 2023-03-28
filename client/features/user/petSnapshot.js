43ee85c6135a521fa7be8fb9f691ad5cc6c77182
Carla Herrera <cgherrera07@gmail.com>
2023-03-28 15:02:24 -0400

    Troubleshooting image

--- client/features/user/petSnapshot.js
+++ client/features/user/petSnapshot.js
@@ -2,18 +2,26 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { selectSingleUser, fetchSingleUser } from "./singleUserSlice";
+import { selectSelectedPet, fetchSelectPetAsync } from "../pet/selectPetSlice";
 
 const PetSnapshot = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
+  const petId = useParams();
 
   const singleUser = useSelector(selectSingleUser);
+  const singlePet = useSelector(selectSelectedPet);
 
   const { selectPets } = singleUser;
+  const { selectImg } = singlePet;
 
   useEffect(() => {
     dispatch(fetchSingleUser(id));
   }, [dispatch, id]);
+
+  useEffect(() => {
+    dispatch(fetchSelectPetAsync(petId));
+  }, [dispatch]);
 
   return (
     <div className="pet-details">
@@ -21,12 +29,16 @@ const PetSnapshot = () => {
         <h2>Pets</h2>
         <hr />
         {selectPets && selectPets.length ? (
-          selectPets.map((pet) => (
-            <div className="petList" key={pet.id}>
+        selectPets.map((pet) => (
+        <div className="petList" key={pet.id}>
               <Link to={`/pets/${pet.id}`}>
-                <h3>Name: {pet.name}</h3>
-              </Link>
-              <p>Age: {pet.age}</p>
+                 <h3>Name: {pet.name}</h3>
+               </Link>
+               <img
+                 className="profilePet"
+                 src={singlePet.pet?.image?.[selectImg]}
+               />
+               <p>Age: {pet.age}</p>
             </div>
           ))
         ) : (
