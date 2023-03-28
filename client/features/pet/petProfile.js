1a6f487bbbbe90f817400d65721b0993d470ced6
Carla Herrera <cgherrera07@gmail.com>
2023-03-28 12:23:26 -0400

    Delete console.logs and commented out code

--- client/features/pet/petProfile.js
+++ client/features/pet/petProfile.js
@@ -21,12 +21,6 @@ const PetProfile = () => {
   };
 
   const { id, name, age, exp, user, selectImg } = singlePet;
-  console.log(singlePet.pet?.species)
-  // const { species, image } = singlePet.pet;
-
-  console.log(petId.id);
-
-  // console.log("current img:", image);
 
   return (
     <div className={styles.PetProfile}>
