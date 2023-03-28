4bc4b65ce191b47187d766ec06f042ad4d3f2735
Carla Herrera <cgherrera07@gmail.com>
2023-03-28 12:30:54 -0400

    Add rough draft copy for logged-out home page

--- client/features/home/LoggedOut.js
+++ client/features/home/LoggedOut.js
@@ -4,15 +4,24 @@ const LoggedOut = () => {
   return (
     <div>
       <h1>Welcome to ToDomagotchi!</h1>
+      <br />
+      <p>
+        Feel unmotivated or unproductive? Is your usual task app bland to look
+        at? Then perhaps To-Domagotchi is for you! With custom illustrations and
+        accessible mechanics, To-Domagotchi provides you with a fun, tangible
+        way to keep up with your daily tasks while watching your pet grow.{" "}
+      </p>
+      <br/>
       <div>
         {/* Choose a pet section */}
         <div>
           <h3>Choose a pet!</h3>
           <div>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-              corrupti ipsam modi at neque itaque, blanditiis fuga eligendi!
-              Porro, totam!
+              Choose a pet to care for and watch it grow the more tasks you
+              complete! When you first start out, you can select a pet category,
+              plants or monsters, and watch as your baby pet is unveiled right
+              before your eyes.
             </p>
           </div>
           <div>
@@ -31,18 +40,18 @@ const LoggedOut = () => {
           <h3>Make your pet grow with productivity!</h3>
           <div>
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-              corrupti ipsam modi at neque itaque, blanditiis fuga eligendi!
-              Porro, totam!
+              For each task you complete, your pet will gain experience points
+              and grow to new stages and forms. But be careful! If you neglect
+              to finish a task before the deadline you set, your pet loses
+              experience points. Be sure to log on every day to ensure your pet
+              stays healthy, or if you need a break, turn on Vacation Mode!
             </p>
             <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
           </div>
           <div>
             <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
             <p>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo
-              corrupti ipsam modi at neque itaque, blanditiis fuga eligendi!
-              Porro, totam!
+              Make checking off todos more fun by caring for your very own ToDomagotchi pet. Sign up today!
             </p>
           </div>
         </div>
