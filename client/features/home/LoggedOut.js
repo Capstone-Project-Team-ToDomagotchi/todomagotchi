874b7a72acd559a39889c18e4dd494836f3833c2
Carla Herrera <cgherrera07@gmail.com>
2023-03-28 22:30:40 -0400

    Edit copy on logged-out home page and edit css to accomodate new text. Add note regarding image section

--- client/features/home/LoggedOut.js
+++ client/features/home/LoggedOut.js
@@ -8,7 +8,7 @@ const LoggedOut = () => {
       {/* Choose a pet section */}
       <section className={styles.petChoice}>
         <div>
-          <h3>Choose a pet!</h3>
+          <h3>Choose a pet</h3>
           <p>
             Choose a pet to care for and watch it grow the more tasks you complete! When you first start out, you can select from two pet categories, plants or monsters, and watch as your baby pet is unveiled right before your eyes.
           </p>
@@ -24,7 +24,7 @@ const LoggedOut = () => {
       </section>
       {/* Make your pet grow with productivity section */}
       <section className={styles.productivity}>
-        <h3>Make your pet grow with productivity!</h3>
+        <h3>Make your pet grow with productivity</h3>
         <p>
           For each task you complete, your pet will gain experience points and grow to new stages and forms. But be careful! If you neglect to finish a task before the deadline you set, your pet loses experience points. Be sure to log on every day to ensure your pet stays healthy, or if you need a break, turn on Vacation Mode!
         </p>
@@ -34,20 +34,18 @@ const LoggedOut = () => {
             <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
           </span>
         </div>
-      </section>
-      {/* Home pic on home page section */}
-      <section className={styles.signUp}>
-          <h3>Sign Up Today!</h3>
-          <p>
-          Make checking off todos more fun by caring for your very own
-          ToDomagotchi pets. You'll find your productivity increase in no time. Sign up today!
-          </p>
-          <div>
-        {/* <section className={styles.image}> */}
-            <span>
-              <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
-            </span>
-          </div>
+      </section> 
+      {/* Home pic of lady with the pets on home page section. It's meant to be a larger picture that will take up the right panel (so there won't be as much white space as there is now) */}
+      <section className={styles.image}>
+      <h3>Sign up today!</h3>
+      <p>
+      Make checking off todos more fun by caring for your very own ToDomagotchi pets!
+      </p>
+        <div>
+          <span>
+            <img src="https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=740&t=st=1679076429~exp=1679077029~hmac=2b890899ac39e6ac38809cccbdd1160d155b9493d8afa43be6ac32afc2358861" />
+          </span>
+        </div>
       </section>
     </main>
   );
