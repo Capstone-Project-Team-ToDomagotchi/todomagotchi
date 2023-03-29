f377a1fabeacc391ab241237b8d2592bba391ce3
Zelda Ogiamien <zel0789@yahoo.com>
2023-03-29 17:36:27 -0400

    was advised that we just need to add the import link to fix the error

--- client/features/user/todosSnapshot.js
+++ client/features/user/todosSnapshot.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link } from "react-router-dom";
 import { selectSingleUser, fetchSingleUser } from "./singleUserSlice";
 
+import styles from "../styles/TodoSnapshot.module.css";
+
 const TodosSnapshot = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
@@ -19,17 +21,17 @@ const TodosSnapshot = () => {
 
   return (
     <main className={styles.todoContainer}>
-    <h2>Current Todos</h2>
-    {todos && todos.length ? (
-      todos.map((todo) => (
-        <section className={styles.todoList} key={todo.id}>
-          <Link to={`/todos/${todo.id}`}>Name: {todo.todoName}</Link>
-          {todos.description}
-        </section>
-      ))
-    ) : (
-      <p>No todos exist for this user</p>
-    )}
+      <h2>Current Todos</h2>
+      {todos && todos.length ? (
+        todos.map((todo) => (
+          <section className={styles.todoList} key={todo.id}>
+            <Link to={`/todos/${todo.id}`}>Name: {todo.todoName}</Link>
+            {todos.description}
+          </section>
+        ))
+      ) : (
+        <p>No todos exist for this user</p>
+      )}
     </main>
   );
 };
