6b7cd8f0ec6d48f625235e1efa22b995d28a6307
Jing Mo <Jingmo39@gmail.com>
2023-03-29 18:45:36 -0400

    nav

--- client/features/todo/todoList.js
+++ client/features/todo/todoList.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import { Link, useNavigate } from "react-router-dom";
+
 import { selectTodo, fetchTodosAsync, toggleCompleted } from "./todoSlice";
 import styles from "../styles/Todos.module.css"
 import { addExpToPet } from "../pet/selectPetSlice";
