ef923079195a7428e65de4c23e4b3f5e6b4fe5b7
sstephens0907 <102310021+sstephens0907@users.noreply.github.com>
2023-03-31 17:47:09 -0400

    update responsive formatting to users page

--- client/features/user/UserPage.js
+++ client/features/user/UserPage.js
@@ -26,10 +26,9 @@ const User = () => {
   const currentUser = useSelector((state) => state.auth.me);
 
   return (
-      <div className={styles.userProfile}>
+      <main className={styles.userProfile}>
           <header id="user-header">
             <img id="user-img" src={profilePic}></img>
-            <div>
               {displayName && <h2>Name: {displayName}</h2>}
               {username && <h3>Username: {username} </h3>}
               {pronouns && <p>Pronouns: {pronouns}</p>}
@@ -37,11 +36,11 @@ const User = () => {
               <br />
               {(currentUser.id === singleUser.id) &&
               <button onClick={() => navigate(`/users/${id}/edit`)}>Edit Profile</button>}
-            </div>
           </header>
-          <hr />
+          <section className="friendsDisplay">
           <FriendsSnapshot />
-      </div>
+          </section>
+      </main>
   );
 };
 
