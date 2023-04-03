2a15286ccb18bf03e1e12bc2e4f6bc9d2f05ef5a
Carla Herrera <cgherrera07@gmail.com>
2023-04-03 15:19:00 -0400

    Delete note and part of copy that are no longer needed

--- client/features/home/LoggedOut.js
+++ client/features/home/LoggedOut.js
@@ -5,17 +5,6 @@ import styles from "../styles/LoggedOut.module.css";
 const LoggedOut = () => {
   return (
     <main className={styles.loggedOut}>
-      {/* {Welcome section} */}
-        <div className={styles.speechBubble}>
-          <p>Welcome to ToDoMagotchi!</p>
-        </div>
-        <br />
-        <div>
-          <img
-            className={styles.speechBubbleImg}
-            src="homepagefigure.png"
-          ></img>
-        </div>
       {/* Choose a pet section */}
       <section className={styles.petChoice}>
         <div>
@@ -37,14 +26,11 @@ const LoggedOut = () => {
         </div>
       </section>
       {/* Make your pet grow with productivity section */}
-      <br />
       <section className={styles.productivity}>
         <h3>Make your pet grow with productivity</h3>
         <p>
           For each task you complete, your pet will gain experience points and
-          grow to new stages and forms. But be careful! If you neglect to finish
-          a task before the deadline you set, your pet loses experience points.
-          Be sure to log on every day to ensure your pet stays healthy!
+          grow to new stages and forms.
         </p>
         <Link to="/signup" className="signUp">
         <h3>Sign up today!</h3>
@@ -54,7 +40,6 @@ const LoggedOut = () => {
           ToDomagotchi pets!
         </p>
       </section>
-      {/* Home pic of lady with the pets on home page section. It's meant to be a larger picture that will take up the right panel (so there won't be as much white space as there is now) */}
       <section className={styles.signUp}>
         <div></div>
       </section>
