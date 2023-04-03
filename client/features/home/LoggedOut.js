c283cb51172ec498577ffc7cdabbf6f30a26b2fa
Carla Herrera <cgherrera07@gmail.com>
2023-04-03 02:06:25 -0400

    Adjust width of new items so they are more responsive to screen size changes

--- client/features/home/LoggedOut.js
+++ client/features/home/LoggedOut.js
@@ -5,19 +5,26 @@ import styles from "../styles/LoggedOut.module.css";
 const LoggedOut = () => {
   return (
     <main className={styles.loggedOut}>
+      {/* {Welcome section} */}
+        <div className={styles.speechBubble}>
+          <p>Welcome to ToDoMagotchi!</p>
+        </div>
+        <br />
+        <div>
+          <img
+            className={styles.speechBubbleImg}
+            src="homepagefigure.png"
+          ></img>
+        </div>
       {/* Choose a pet section */}
-      <div className={styles.speechBubble}>
-        <p>Welcome to ToDoMagotchi!</p>
-      </div>
-      <br/>
-      <div>
-        <img className={styles.speechBubbleImg} src="homepagefigure.png"></img>
-      </div>
       <section className={styles.petChoice}>
         <div>
           <h3>Choose a pet</h3>
           <p>
-            Choose a pet to care for and watch it grow the more tasks you complete! When you first start out, you can select from two pet categories, plants or monsters, and watch as your baby pet is unveiled right before your eyes.
+            Choose a pet to care for and watch it grow the more tasks you
+            complete! When you first start out, you can select from two pet
+            categories, plants or monsters, and watch as your baby pet is
+            unveiled right before your eyes.
           </p>
           <span>
             <p>Monster</p>
@@ -29,18 +36,22 @@ const LoggedOut = () => {
           </span>
         </div>
       </section>
-      {/* Make your pet grow with productivity section */}     
-      <br/>
+      {/* Make your pet grow with productivity section */}
+      <br />
       <section className={styles.productivity}>
         <h3>Make your pet grow with productivity</h3>
         <p>
-          For each task you complete, your pet will gain experience points and grow to new stages and forms. But be careful! If you neglect to finish a task before the deadline you set, your pet loses experience points. Be sure to log on every day to ensure your pet stays healthy!
+          For each task you complete, your pet will gain experience points and
+          grow to new stages and forms. But be careful! If you neglect to finish
+          a task before the deadline you set, your pet loses experience points.
+          Be sure to log on every day to ensure your pet stays healthy!
         </p>
         <h3>Sign up today!</h3>
         <p>
-          Make checking off todos more fun by caring for your very own ToDomagotchi pets!
+          Make checking off todos more fun by caring for your very own
+          ToDomagotchi pets!
         </p>
-      </section> 
+      </section>
       {/* <section className={styles.signUp}>
         <div>
         </div>
